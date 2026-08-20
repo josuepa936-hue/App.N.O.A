9aacfc8b843edee8d7fea59eb7f353a2873a5f4f
josuepa936-hue <josuepa936@gmail.com>
2026-08-20 02:02:23 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='noa-mobile-v6-1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/noa-192.png','./icons/noa-512.png'];
+const ASSETS=['./','./index.html','./manifest.webmanifest','./noa-192.png','./noa-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
