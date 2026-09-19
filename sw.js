75f14772bee86965af8aa3824b868267958ba588
josuepa936-hue <josuepa936@gmail.com>
2026-09-18 22:55:28 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE =
-  'noa-mobile-v6-48';
+  'noa-mobile-v6-49';
 
 
 const ASSETS = [
@@ -47,6 +47,8 @@ const ASSETS = [
   './excoba_patch_v19_drag_order_mixed.js',
 
   './excoba_patch_v20_exam_shell.js',
+
+  './excoba_patch_v21_exam_navigation.js',
 
   './noa-192.png',
 
